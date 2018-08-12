76d4176ad55d2a66092a43e63ecbb7065deecfa2
mroman <me@mroman.ch>
2018-08-12 14:45:58 +0200

    + add few more tests

--- token_test.go
+++ token_test.go
@@ -9,6 +9,9 @@ func TestEmpty(t *testing.T) {
 
 func TestSimple(t *testing.T) {
     checkTypes("func", []TokenType{TT_FUNC}, t)
+    checkTypes("  func", []TokenType{TT_FUNC}, t)
+    checkTypes(" func ", []TokenType{TT_FUNC}, t)
+    checkTypes(" func\n func\n ", []TokenType{TT_FUNC, TT_FUNC}, t)
 }
 
 func checkTypes(str string, tts []TokenType, t *testing.T) {
