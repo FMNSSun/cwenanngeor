f96071906d8528666a47df9ed843dd5686b1422b
mroman <me@mroman.ch>
2018-08-12 14:52:21 +0200

    + add parens

--- char.go
+++ char.go
@@ -1,21 +1,21 @@
 package cwenanngeor
 
 func isident(rn rune) bool {
-    return isletter(rn)
+	return isletter(rn)
 }
 
 func isletter(rn rune) bool {
-    if (rn >= 'a' && rn <= 'z') {
-        return true
-    }
-    
-    if (rn >= 'A' && rn <= 'Z') {
-        return true
-    }
-    
-    return false
+	if rn >= 'a' && rn <= 'z' {
+		return true
+	}
+
+	if rn >= 'A' && rn <= 'Z' {
+		return true
+	}
+
+	return false
 }
 
 func iswhitespace(rn rune) bool {
-    return rn == '\t' || rn == ' '|| rn == '\r' || rn == '\n'
+	return rn == '\t' || rn == ' ' || rn == '\r' || rn == '\n'
 }
--- token_test.go
+++ token_test.go
@@ -1,45 +1,51 @@
 package cwenanngeor
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestEmpty(t *testing.T) {
 }
 
 func TestSimple(t *testing.T) {
-    checkTypes("func", []TokenType{TT_FUNC}, t)
-    checkTypes("  func", []TokenType{TT_FUNC}, t)
-    checkTypes(" func ", []TokenType{TT_FUNC}, t)
-    checkTypes(" func\n func\n ", []TokenType{TT_FUNC, TT_FUNC}, t)
+	checkTypes("func", []TokenType{TT_FUNC}, t)
+	checkTypes("  func", []TokenType{TT_FUNC}, t)
+	checkTypes(" func ", []TokenType{TT_FUNC}, t)
+	checkTypes(" func\n func\n ", []TokenType{TT_FUNC, TT_FUNC}, t)
+}
+
+func TestSpecials(t *testing.T) {
+	checkTypes("{}", []TokenType{TT_BEGIN, TT_END}, t)
+	checkTypes("func()", []TokenType{TT_FUNC, TT_LPAREN, TT_RPAREN}, t)
+	checkTypes(" ; ", []TokenType{TT_SEMICOLON}, t)
 }
 
 func checkTypes(str string, tts []TokenType, t *testing.T) {
-    tz := NewTokenizerString(str)
-    
-    for _, v := range tts {
-        tk, err := tz.Next()
-        
-        if err != nil {
-            t.Fatalf("Unexpected error: %q", err.Error())
-            return
-        }
-        
-        if tk.Type != v {
-            t.Fatalf("Expected TT %d but got %d", v, tk.Type)
-            return
-        }
-    }
-    
-    tk, err := tz.Next()
-        
-    if err != nil {
-        t.Fatalf("Unexpected error: %q", err.Error())
-        return
-    }
-    
-    if tk != nil {
-        t.Fatalf("Expected EOF but still got a token")
-        return
-    }
+	tz := NewTokenizerString(str)
+
+	for _, v := range tts {
+		tk, err := tz.Next()
+
+		if err != nil {
+			t.Fatalf("Unexpected error: %q", err.Error())
+			return
+		}
+
+		if tk.Type != v {
+			t.Fatalf("Expected TT %d but got %d", v, tk.Type)
+			return
+		}
+	}
+
+	tk, err := tz.Next()
+
+	if err != nil {
+		t.Fatalf("Unexpected error: %q", err.Error())
+		return
+	}
+
+	if tk != nil {
+		t.Fatalf("Expected EOF but still got a token")
+		return
+	}
 }
