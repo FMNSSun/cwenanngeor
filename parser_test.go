6918800e6cc56917fa6235aab6fdcfc61dcc89a8
Roman Müntener <me@mroman.ch>
2018-08-14 11:45:45 +0200

    + more tests for parser

--- parser_test.go
+++ parser_test.go
@@ -5,22 +5,40 @@ import (
 )
 
 func TestParseSExp(t *testing.T) {
-	code := "(foo 5 6)"
 
+	checkAST(
+		"(foo 5 6)",
+		&SExpNode{
+			FuncName: "foo",
+			Exps: []Node{
+				&LitIntNode{SVal: "5"},
+				&LitIntNode{SVal: "6"},
+			},
+		}, t)
+
+	checkAST(
+		"(foo 5 (bar 1))",
+		&SExpNode{
+			FuncName: "foo",
+			Exps: []Node{
+				&LitIntNode{SVal: "5"},
+				&SExpNode{
+					FuncName: "bar",
+					Exps: []Node{
+						&LitIntNode{SVal: "1"},
+					},
+				},
+			},
+		}, t)
+}
+
+func checkAST(code string, exp Node, t *testing.T) {
 	p := NewParser(NewTokenizerString(code))
 
 	n, err := p.parseSExp()
 
 	if err != nil {
 		t.Fatalf("Unexpected error: %s.", err.Error())
-	}
-
-	exp := &SExpNode{
-		FuncName: "foo",
-		Exps: []Node{
-			&LitIntNode{SVal: "5"},
-			&LitIntNode{SVal: "6"},
-		},
 	}
 
 	if !ASTEqual(n, exp) {
